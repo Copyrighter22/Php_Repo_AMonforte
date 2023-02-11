b1341f87e1b2ff9d126cf4e72a2ced2810d3608f
«Andreu <andreump5@gmail.com>
2023-02-11 13:24:16 +0100

    v105 - 6:09 min Falta solucionar el error de ocultar .env i /vendor Refactorització del helpers.php

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -10,6 +10,7 @@ class ComposerStaticInitced21377248fe9a23e5633bdd3077430
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
+        '378a6041d621800a4673eab5bfac2a99' => __DIR__ . '/../..' . '/framework/helpers.php',
     );
 
     public static $prefixLengthsPsr4 = array (
